cfcdc00a0b1a682421e3b6607e733f0762647fe6
Black Forest Digital <mail@black-forest-digital.de>
2020-08-13 09:23:08 +0200

    label for textfield

--- resources/views/components/forms/text.blade.php
+++ resources/views/components/forms/text.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if($label ?? null)
-    <label for="{{ $id }}" class="block text-sm font-medium leading-5 text-gray-700">Email</label>
+    <label for="{{ $id }}" class="block text-sm font-medium leading-5 text-gray-700">{{ $label }}</label>
     @endif
     <div class="mt-1 relative rounded-md shadow-sm">
         <input {{ $attributes }} type="{{ $type ?? 'text' }}" id="{{ $id }}" class="form-input bg-cool-gray-100 border-3 border-cool-gray-100 focus:border-3 focus:border-brand-300 w-full px-3 py-3 rounded-md shadow-sm focus:outline-none text-gray-600 font-medium bg-white transition duration-150 ease-in-out @error({{ $id }}) border-red-500 @enderror" placeholder="{{ $placeholder ?? '' }}" {{ ($required ?? false) ? 'required' : '' }}>
